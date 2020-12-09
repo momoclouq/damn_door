a6b817e3de80fe2e58db1104515467514e551242
momoclouq <57221289+momoclouq@users.noreply.github.com>
2020-12-09 21:10:47 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 function DetectDoor () {
     if (!(inRange(currentDirection))) {
         serial.writeLine("\"Door is moving\"")
+        radio.sendString("moving")
         basic.pause(1000)
     } else {
         for (let index = 0; index < 4; index++) {
@@ -8,6 +9,7 @@ function DetectDoor () {
             currentDirection = Math.ceil(input.compassHeading())
             if (!(inRange(currentDirection))) {
                 serial.writeLine("\"Door is still moving\"")
+                radio.sendString("still moving")
                 break;
             } else {
                 stopTime2 += 1
@@ -15,6 +17,7 @@ function DetectDoor () {
         }
         if (stopTime2 == 4) {
             serial.writeLine("\"Door is closed")
+            radio.sendString("closed")
             basic.pause(1000)
         }
         stopTime2 = 0
@@ -44,13 +47,14 @@ let stopTime2 = 0
 let currentDirection = 0
 let start = 0
 start = 0
-radio.setGroup(1)
+radio.setGroup(24)
 basic.forever(function () {
     if (start == 1) {
         currentDirection = Math.ceil(input.compassHeading())
         DetectDoor()
     } else {
         serial.writeLine("\"Device is not running\"")
+        radio.sendString("not running")
         basic.pause(1000)
     }
 })
